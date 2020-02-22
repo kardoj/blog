04781b36d6360017512fe20694fcb5852f32c4c1
kardoj <kardoj@gmail.com>
2020-02-23 00:04:44 +0200

    Create posts page prototype

---
+++ Blog.Web/Controllers/PrototypeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Web.Controllers
+{
+    public class PrototypeController : Controller
+    {
+        public IActionResult LatestPosts()
+        {
+            return View();
+        }
+    }
+}
